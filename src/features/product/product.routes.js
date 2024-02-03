9e518ab597c5ab87e3ad07a083425416df05cc87
alqamah <alqama.hasnain@gmail.com>
2024-02-04 02:33:11 +0530

    jwt-auth implemented

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -4,12 +4,14 @@ import express from 'express'
 import ProductController from './product.controller.js';
 import {upload} from '../../middlewares/file-upload.middleware.js';
 import basicAuth from '../../middlewares/basic-auth.middleware.js';
+import jwtAuth from '../../middlewares/jwt-auth.middleware.js';
 
 const router = express.Router();
 
 const productController = new ProductController();
 
-router.use(basicAuth);
+//router.use(basicAuth);
+router.use(jwtAuth);
 
 router.get('/', productController.getAllProduct);
 router.post('/', upload.single('imageUrl'),productController.addProduct);
