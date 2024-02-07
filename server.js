cbe68023732ab0487179e4ca72c04e295d84cbb0
alqamah <alqama.hasnain@gmail.com>
2024-02-07 12:49:53 +0530

    API documentation created using swagger-ui

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const server = express();
 import cookieParser from "cookie-parser";
+
+import swagger from 'swagger-ui-express';
+import apiDocs from './swagger.json' assert {type:'json'};
 
 import CartRouter from './src/features/cart/cart.routes.js'
 import ProductRouter from './src/features/product/product.routes.js';
 import UserRouter from './src/features/user/user.routes.js';
 
+const server = express();
+
+server.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
 
 server.use(bodyParser.json());
 server.use(cookieParser());
