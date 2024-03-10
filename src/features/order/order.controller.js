21b71fe16ff2340c54bb34effa47b49e1a23278c
alqamah <alqama.hasnain@gmail.com>
2024-03-10 21:59:54 +0530

    order creation commit

--- src/features/order/order.controller.js
+++ src/features/order/order.controller.js
@@ -10,7 +10,7 @@ export default class OrderController{
         try{
         const userId = req.cookies.uid;
             const amt = await this.orderRepository.placeOrder(userId);
-            res.status(201).send(`Order placed successfully. Total amount: Rs.${amt}`);
+            res.status(201).send(`Order placed successfully.`);
         }catch(err){
             console.log(err);
             return res.status(200).send("Something went wrong");
--- src/features/order/order.repository.js
+++ src/features/order/order.repository.js
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import { getDb } from "../../config/mongodb.js";
+import orderModel from "./order.model.js";
 
 export default class OrderRepository{
     constructor(){
@@ -7,13 +8,35 @@ export default class OrderRepository{
     }
 
     async placeOrder(userId){
-
+        const db = getDb();
         // 1. Get cartitems and calculate total amount.
-        const amt = await this.getTotalAmount(userId);
-        return amt;
+        const items = await this.getTotalAmount(userId);
+        const amt = items.reduce((acc, item)=>acc+item.totalAmount, 0);
+        console.log("Total amount: ", amt);
+        
         // 2. Create an order record.
-
+        const order = new orderModel(userId, amt, new Date());
+        await db.collection(this.collection).insertOne(order);
+        console.log("--order record created--");
+        
         // 3. Reduce the stock.
+        for (let item of items){
+            await db.collection("products").updateOne(
+                {_id: new ObjectId(item.productID)},
+                {$inc:{stock: -item.quantity}}
+            )  
+        }
+        console.log("--stock updated--");
+        let updatedProductStockDetails = [];
+        for (let item of items){
+            let updatedStock = await db.collection("products").findOne({_id: new ObjectId(item.productID)});
+            let item2 = {
+                productID: item.productID,
+                stock: updatedStock.stock
+            }
+            updatedProductStockDetails.push(item2);
+        }
+        console.log("Stock updation details:", updatedProductStockDetails);
 
         // 4. Clear the cart items.
     }
@@ -47,9 +70,19 @@ export default class OrderRepository{
                 }
             }
         ]).toArray();
-        //console.log(items);
-        const finalTotalAmount = items.reduce((acc, item)=>acc+item.totalAmount, 0)
-        console.log("Total Amount:",finalTotalAmount);
-        return finalTotalAmount;
+
+        let orderDetails = [];
+        
+        for(let item of items){
+            let products = {
+                productID : item.productInfo._id,
+                price : item.productInfo.price,
+                stock : item.productInfo.stock,
+                quantity : item.quantity,
+            };
+            orderDetails.push(products);
+        }
+        console.log("Order Details:", orderDetails);
+        return items;
     }
 }
